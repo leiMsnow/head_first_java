68cb3e6fee8fc817fa92b6c58e999b84e65c303b
Ray <always.0122@gmail.com>
2017-08-23 10:25:26 +0800

    aaa

--- src/com/ray/java/leetcode/LongestPalindromicSubstring.java
+++ src/com/ray/java/leetcode/LongestPalindromicSubstring.java
@@ -4,4 +4,32 @@ package com.ray.java.leetcode;
  * Created by dangdang on 2017/8/18.
  */
 public class LongestPalindromicSubstring {
+
+    public static void main(String[] args) {
+        String s = "aba";
+        System.out.println("longestPalindrome: " + longestPalindrome(s));
+    }
+
+    private static String longestPalindrome(String s) {
+        int start = 0;
+        int end = 0;
+        for (int i = 0; i < s.length(); i++) {
+            int len1 = expandAroundCenter(s, i, i);
+            int len2 = expandAroundCenter(s, i, i + 1);
+            int len = Math.max(len1, len2);
+            if (len > end - start) {
+                start = i - (len - 1) / 2;
+                end = i + len / 2;
+            }
+        }
+        return s.substring(start, end + 1);
+    }
+
+    private static int expandAroundCenter(String s, int L, int R) {
+        while (L >= 0 && R < s.length() && s.charAt(L) == s.charAt(R)) {
+            L--;
+            R++;
+        }
+        return R - L - 1;
+    }
 }
